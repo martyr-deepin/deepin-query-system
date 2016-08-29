e824d252e83630584de9d2e18a8bd3afc1eca5f8
Choldrim <choldrim@foxmail.com>
2016-08-29 13:52:36 +0800

    Fetch code automatically before launch server
    Change-Id: I748995c1038314a86b1c17a4c082275a24dc65b2

--- sub-systems/repository/entrypoint.sh
+++ sub-systems/repository/entrypoint.sh
@@ -1,5 +1,15 @@
-#!/bin/bash
+#!/bin/bash -e
+cd /data/deepin-query-system
+
+# fetch source
+git fetch
+git checkout origin/master
+
+# cover the system sources.list
+cp sub-systems/repository/sources.list /etc/apt/sources.list
+
+# update apt list before launching
 apt-get update
 
-cd /data/source
-python3 repository-server.py
+# launch
+python3 sub-systems/repository/source/repository-server.py
